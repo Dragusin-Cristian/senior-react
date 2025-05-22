276f5201a01de092f4d477e7016c8371478a7cd5
Dragusin Cristian <dragusincristian609@gmail.com>
2025-05-22 17:03:28 +0300

    ultimate generic container component: data source loader

--- react-design-patterns/src/App.js
+++ react-design-patterns/src/App.js
@@ -3,6 +3,13 @@ import { UserInfo } from "./components/user-info";
 import UserLoader from "./components/user-loader";
 import ResourceLoader from "./components/resource-loader";
 import { BookInfo } from "./components/book-info";
+import DataSource from "./components/data-source";
+import axios from "axios";
+
+const getDataFromServer = async (url) => {
+  const response = await axios.get(url);
+  return response.data;
+};
 
 function App() {
   return (
@@ -19,13 +26,20 @@ function App() {
       </UserLoader> */}
 
       {/* ********** Generic Resource Loader ********** */}
-      <ResourceLoader resourceUrl="/users/2" resourceName="user">
+      {/* <ResourceLoader resourceUrl="/users/2" resourceName="user">
         <UserInfo />
       </ResourceLoader>
-
       <ResourceLoader resourceUrl="/books/2" resourceName="book">
         <BookInfo />
-      </ResourceLoader>
+      </ResourceLoader> */}
+
+      {/* ********** Generic Data Source ********** */}
+      <DataSource
+        getData={getDataFromServer.bind(this, "/users/3")}
+        resourceName="user"
+      >
+        <UserInfo />
+      </DataSource>
     </>
   );
 }
