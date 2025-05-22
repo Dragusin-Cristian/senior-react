14fd04f2938398a95d46d2741a572f7e0fe237b2
Dragusin Cristian <dragusincristian609@gmail.com>
2025-05-22 16:49:57 +0300

    getting specific users by id

--- react-design-patterns/src/App.js
+++ react-design-patterns/src/App.js
@@ -1,11 +1,20 @@
 import CurrentUserLoader from "./components/current-user-loader";
-import UserInfo from "./components/user-info";
+import { UserInfo } from "./components/user-info";
+import UserLoader from "./components/user-loader";
 
 function App() {
   return (
-    <CurrentUserLoader>
-      <UserInfo />
-    </CurrentUserLoader>
+    <>
+      <UserLoader userId={1}>
+        <UserInfo />
+      </UserLoader>
+      <UserLoader userId={2}>
+        <UserInfo />
+      </UserLoader>
+      <UserLoader userId={3}>
+        <UserInfo />
+      </UserLoader>
+    </>
   );
 }
 
