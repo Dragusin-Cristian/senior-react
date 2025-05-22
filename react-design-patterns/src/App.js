cce010e16822920fdf19a1ea9866d616f6ddcd20
Dragusin Cristian <dragusincristian609@gmail.com>
2025-05-22 16:56:20 +0300

    generic resource container

--- react-design-patterns/src/App.js
+++ react-design-patterns/src/App.js
@@ -1,11 +1,14 @@
 import CurrentUserLoader from "./components/current-user-loader";
 import { UserInfo } from "./components/user-info";
 import UserLoader from "./components/user-loader";
+import ResourceLoader from "./components/resource-loader";
+import { BookInfo } from "./components/book-info";
 
 function App() {
   return (
     <>
-      <UserLoader userId={1}>
+      {/* ********** Specific User Loader ********** */}
+      {/* <UserLoader userId={1}>
         <UserInfo />
       </UserLoader>
       <UserLoader userId={2}>
@@ -13,7 +16,16 @@ function App() {
       </UserLoader>
       <UserLoader userId={3}>
         <UserInfo />
-      </UserLoader>
+      </UserLoader> */}
+
+      {/* ********** Generic Resource Loader ********** */}
+      <ResourceLoader resourceUrl="/users/2" resourceName="user">
+        <UserInfo />
+      </ResourceLoader>
+
+      <ResourceLoader resourceUrl="/books/2" resourceName="book">
+        <BookInfo />
+      </ResourceLoader>
     </>
   );
 }
