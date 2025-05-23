9b839d0dafda007725271bac5a34369796184553
Dragusin Cristian <dragusincristian609@gmail.com>
2025-05-23 16:29:28 +0300

    controlled modal

--- react-design-patterns/src/App.js
+++ react-design-patterns/src/App.js
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import ControlledForm from "./components/controlled-form";
+import { ControlledModal } from "./components/controlled-modal";
 import UncontrolledForm from "./components/uncontrolled-form";
 
 function App() {
+  const [shouldDisplay, setShouldDisplay] = useState(false);
+
   return (
-    <div>
+    <>
       {/* <UncontrolledForm /> */}
-      <ControlledForm />
-    </div>
+      {/* <ControlledForm /> */}
+
+      <ControlledModal
+        shouldShow={shouldDisplay}
+        close={() => setShouldDisplay(false)}
+      >
+        <h3>I am the body of the modal!</h3>
+      </ControlledModal>
+      <button onClick={() => setShouldDisplay(true)}>Show modal</button>
+    </>
   );
 }
 
