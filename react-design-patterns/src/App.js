9b007cde63468f5923dc7efe99ca59c5f9e7fe82
Dragusin Cristian <dragusincristian609@gmail.com>
2025-05-23 20:37:43 +0300

    load data with HOC

--- react-design-patterns/src/App.js
+++ react-design-patterns/src/App.js
@@ -1,15 +1,19 @@
+import { includeUser } from "./components/include-user";
 import { logProps } from "./components/log-props";
 import { UserInfo } from "./components/user-info";
 
-const UserInfoWrapper = logProps(UserInfo);
+// const UserInfoWrapper = logProps(UserInfo);
+const UserInfoWithLoader = includeUser(UserInfo, "3");
 
 function App() {
   return (
     <>
-      <UserInfoWrapper
+      {/* <UserInfoWrapper
         test={"Some test prop here"}
         user={{ name: "Criss", age: 23, country: "Romania", books: [] }}
-      />
+      /> */}
+
+      <UserInfoWithLoader />
     </>
   );
 }
