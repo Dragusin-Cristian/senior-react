4d1f55e50cd35e97c54239361290ac25e6c875b2
Dragusin Cristian <dragusincristian609@gmail.com>
2025-05-23 16:38:57 +0300

    uncontrolled flow: onboarding steps flow

--- react-design-patterns/src/App.js
+++ react-design-patterns/src/App.js
@@ -3,6 +3,32 @@ import "./App.css";
 import ControlledForm from "./components/controlled-form";
 import { ControlledModal } from "./components/controlled-modal";
 import UncontrolledForm from "./components/uncontrolled-form";
+import UncontrolledFlow from "./components/uncontrolled-flow";
+
+const StepOne = ({ goNext }) => {
+  return (
+    <>
+      <h1>Step #1</h1>
+      <button onClick={goNext}>Next</button>
+    </>
+  );
+};
+const StepTwo = ({ goNext }) => {
+  return (
+    <>
+      <h1>Step #2</h1>
+      <button onClick={goNext}>Next</button>
+    </>
+  );
+};
+const StepThree = ({ goNext }) => {
+  return (
+    <>
+      <h1>Step #3</h1>
+      <button onClick={goNext}>Next</button>
+    </>
+  );
+};
 
 function App() {
   const [shouldDisplay, setShouldDisplay] = useState(false);
@@ -12,13 +38,20 @@ function App() {
       {/* <UncontrolledForm /> */}
       {/* <ControlledForm /> */}
 
-      <ControlledModal
+      {/* Controlled modal */}
+      {/* <ControlledModal
         shouldShow={shouldDisplay}
         close={() => setShouldDisplay(false)}
       >
         <h3>I am the body of the modal!</h3>
       </ControlledModal>
-      <button onClick={() => setShouldDisplay(true)}>Show modal</button>
+      <button onClick={() => setShouldDisplay(true)}>Show modal</button> */}
+
+      <UncontrolledFlow>
+        <StepOne />
+        <StepTwo />
+        <StepThree />
+      </UncontrolledFlow>
     </>
   );
 }
