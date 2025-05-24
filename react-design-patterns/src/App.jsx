4de2b9277f6cfce1e8244566474f12cb3abcb8e5
Dragusin Cristian <dragusincristian609@gmail.com>
2025-05-24 15:27:32 +0300

    cleaned up prev section

--- react-design-patterns/src/App.jsx
+++ react-design-patterns/src/App.jsx
@@ -1,14 +1,10 @@
 
 import "./App.css";
-import Child from "./child";
-import { ErrorBoundry } from "./error-boundry";
 
 function App() {
 
   return (
     <>
-      <h1>Parent Component</h1>
-      <ErrorBoundry fallback={<h1>Error at child level</h1>}><Child /></ErrorBoundry>
     </>
   );
 }
--- react-design-patterns/src/index.js
+++ react-design-patterns/src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ErrorBoundry } from "./error-boundry";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ErrorBoundry fallback={<h1>Error at App Level</h1>}>
-      <App />
-    </ErrorBoundry>
+    <App />
   </React.StrictMode>
 );
 
