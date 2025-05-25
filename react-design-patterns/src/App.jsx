110fc2efda35dad13fa6fb12473504d72dcd565b
Dragusin Cristian <dragusincristian609@gmail.com>
2025-05-25 15:22:05 +0300

    useLayoutEffect

--- react-design-patterns/src/App.jsx
+++ react-design-patterns/src/App.jsx
@@ -1,38 +1,42 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+
+
 
 function App() {
-  const [show, setShow] = useState(false);
+
+  const [number, setNumber] = useState(0)
+  const [sectionStyle, setSectionStyle] = useState({})
+  const sectionRef = useRef()
+
+  /* 
+  useEffect is asynchronous. 
+  You see the number change in the DOM before the padding changes. 
+
+  useLayoutEffect is synchronous. 
+  You see the number change only after the padding has changed.
+  */
+
+  useLayoutEffect(() => {
+    const random = Math.floor(Math.random() * 500)
+
+    const bigNumber = 100000000
+
+    for(let i = 0; i<= bigNumber; i++) {
+      if(i === bigNumber) setSectionStyle({marginTop: `${random}px`})
+    }
+  }, [number])
+  
 
   return (
-    <div 
-    // onClick={console.log("Outer div clicked")} 
-    onClickCapture={() => console.log("Outer div clicked")}
-    >
-      <h1>Other Content</h1>
-      <button onClick={() => setShow(true)}>Show Message</button>
-      <Alert show={show} onClose={() => setShow(false)}>
-        A sample message to show.
-        <br />
-        Click it to close.
-      </Alert>
+    <div className="App">
+      <section ref={sectionRef} style={sectionStyle}>
+        <p>{number}</p>
+        <button onClick={() => setNumber(current => current -1)}>-</button>
+        <button onClick={() => setNumber(current => current +1)}>+</button>
+      </section>
     </div>
   );
 }
 
-const Alert = ({ children, onClose, show }) => {
-  if (!show) return;
-
-  return (
-    <div className="alert" onClick={() => {
-        onClose();
-        // console.log("Inner div clicked")
-      }}
-      onClickCapture={() => console.log("Inner div clicked")}
-      >
-      {children}
-    </div>
-  );
-};
 
 export default App;
