ca01a293ea07fea0323d3083792f86e504ba557c
Dragusin Cristian <dragusincristian609@gmail.com>
2025-05-25 15:34:44 +0300

    useLayoutEffect code cleaned

--- react-design-patterns/src/App.jsx
+++ react-design-patterns/src/App.jsx
@@ -1,39 +1,13 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import React from "react";
 
 
 
 function App() {
 
-  const [number, setNumber] = useState(0)
-  const [sectionStyle, setSectionStyle] = useState({})
-  const sectionRef = useRef()
-
-  /* 
-  useEffect is asynchronous. 
-  You see the number change in the DOM before the padding changes. 
-
-  useLayoutEffect is synchronous. 
-  You see the number change only after the padding has changed.
-  */
-
-  useLayoutEffect(() => {
-    const random = Math.floor(Math.random() * 500)
-
-    const bigNumber = 100000000
-
-    for(let i = 0; i<= bigNumber; i++) {
-      if(i === bigNumber) setSectionStyle({marginTop: `${random}px`})
-    }
-  }, [number])
   
-
   return (
     <div className="App">
-      <section ref={sectionRef} style={sectionStyle}>
-        <p>{number}</p>
-        <button onClick={() => setNumber(current => current -1)}>-</button>
-        <button onClick={() => setNumber(current => current +1)}>+</button>
-      </section>
+     
     </div>
   );
 }
