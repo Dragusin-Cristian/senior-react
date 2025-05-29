a39f9c4611814285cb28012b958cdf01942e6f5a
Dragusin Cristian <dragusincristian609@gmail.com>
2025-05-29 12:15:14 +0300

    element (As) prop

--- react-design-patterns/src/components/button.jsx
+++ react-design-patterns/src/components/button.jsx
@@ -1,8 +1,8 @@
 import styles from "./button.module.css";
 
-const Button = ({ size = "m", className = "", ...otherProps }) => {
+const Button = ({ As = "button", size = "m", className = "", ...otherProps }) => {
   return (
-    <button
+    <As
       {...otherProps}
       className={`${styles.button} ${styles[size]} ${className}`}
     />
