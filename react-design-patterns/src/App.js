046c2a5013ba16be7b3c6411091676616e5ee04b
Dragusin Cristian <dragusincristian609@gmail.com>
2025-06-08 19:34:04 +0300

    removed code from prev section

--- react-design-patterns/src/App.js
+++ react-design-patterns/src/App.js
@@ -1,24 +1,5 @@
-import { useState } from "react";
-import TasksBoard from "./components/tasks-board";
-
 function App() {
-  // const [person, setPerson] = useState({
-  //   name: "William",
-  // });
-
-  // const onChange = () => {
-  //   // ❌ This won't work
-  //   person.age = 24;
-
-  //   // ✅ This will work
-  //   setPerson({ ...person, age: 24 });
-  // };
-
-  return (
-    <>
-      <TasksBoard />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
