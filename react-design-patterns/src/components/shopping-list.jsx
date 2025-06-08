a5cbc079476e839846e1cf00b99961cdaedb0f1d
Dragusin Cristian <dragusincristian609@gmail.com>
2025-06-08 22:31:53 +0300

    useImmerReducer

--- react-design-patterns/src/components/shopping-list.jsx
+++ react-design-patterns/src/components/shopping-list.jsx
@@ -1,7 +1,8 @@
-import React, { useReducer } from "react";
+import React from "react";
 import ShoppingListHeader from "./shopping-list-header";
 import ShoppingListRow from "./shopping-list-row";
 import styled from "styled-components";
+import {useImmerReducer} from "use-immer"
 
 const StyledContainer = styled.div`
   padding-top: 2rem;
@@ -62,38 +63,25 @@ const shoppingItems = {
 const reducer = (state, action) => {
   switch (action.type) {
     case "UPDATE_NEW_SHOPPING_ITEM_NAME":
-      return {
-        ...state,
-        newShoppingItemName: action.payload,
-      };
+      state.newShoppingItemName = action.payload;
+      break;
     case "ADD_ITEM":
-      return {
-        ...state,
-        newShoppingItemName: "",
-        items: [...state.items, action.payload],
-      };
+      state.newShoppingItemName = "";
+      state.items.push(action.payload);
+      break;
     case "UPDATE_ITEM":
-      return {
-        ...state,
-        items: state.items.map((item, idx) => {
-          if (idx === action.payload.index) {
-            return action.payload.item;
-          }
-          return item;
-        }),
-      };
+      state.items.splice(action.payload.index, 1, action.payload.item);
+      break;
     case "DELETE_ITEM":
-      return {
-        ...state,
-        items: state.items.filter((_, idx) => idx !== action.payload.index),
-      };
-    default:
-      return state;
+      state.items.splice(action.payload.index, 1);
+      break;
+    default: break;
   }
+  return state;
 };
 
 const ShoppingList = (props) => {
-  const [shoppingList, dispatch] = useReducer(reducer, shoppingItems);
+  const [shoppingList, dispatch] = useImmerReducer(reducer, shoppingItems);
   const addItem = () => {
     if (!shoppingList.newShoppingItemName) return;
     dispatch({
