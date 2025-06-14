097d26e198b0f7714300d8fd89c5a4aefb56b1c1
Dragusin Cristian <dragusincristian609@gmail.com>
2025-06-14 19:42:17 +0300

    extensible foundations

--- react-design-patterns/src/index.js
+++ react-design-patterns/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { GlobalStyles } from "./utils";
 
 const App = () => {
   return (
     <>
       <h1>Let's build some components</h1>
+
+      <GlobalStyles />
     </>
   );
 };
