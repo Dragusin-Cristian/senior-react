fe989fd8d059eb1d9d398711df33918082b46ddf
Dragusin Cristian <dragusincristian609@gmail.com>
2025-06-19 15:59:42 +0300

    "as const" for typing hooks returned

--- react-design-patterns/src/App.tsx
+++ react-design-patterns/src/App.tsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
+const useUrl = (defaultUrl: string) => {
+  const [url, setUrl] = useState(defaultUrl);
+
+  return [url, setUrl] as const; // this cast into const will keep the type outside the hook just like it is inside the hook
+};
+
 function App() {
+  const [url, setUrl] = useUrl("www.google.com");
+
   return <></>;
 }
 
