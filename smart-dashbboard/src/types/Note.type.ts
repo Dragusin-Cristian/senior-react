34ce5d69a04240189533d56577b8ed8bb8d5318a
Dragusin Cristian <dragusincristian609@gmail.com>
2025-07-08 12:10:48 +0300

    user and note types

---
+++ smart-dashbboard/src/types/Note.type.ts
@@ -0,0 +1,10 @@
+import TUser from "./User.type";
+
+type TNote  = {
+    id: number,
+    title: string,
+    text: string,
+    userId: number,
+}
+
+export default TNote;
---
+++ smart-dashbboard/src/types/User.type.ts
@@ -0,0 +1,6 @@
+type TUser = {
+    id: number;
+    name: string;
+};
+
+export default TUser;
