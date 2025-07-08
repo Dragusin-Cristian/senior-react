cfcbfa52a9c9065d9fa89405cd2492d109a1a610
Dragusin Cristian <dragusincristian609@gmail.com>
2025-07-08 12:10:38 +0300

    show the dashboard view

--- smart-dashbboard/src/App.tsx
+++ smart-dashbboard/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import DashboardView from "./views/Dashboard.view";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <DashboardView />
+    </div>
+  );
 }
 
 export default App;
---
+++ smart-dashbboard/src/views/Dashboard.view.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DashboardContainer from "../components/DashboardContainer";
+
+const DashboardView = () => {
+  return (
+    <div>
+      {/* layout to be wrapped around, with a nav bar and stuff */}
+      <DashboardContainer />
+    </div>
+  );
+};
+
+export default DashboardView;
