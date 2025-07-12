2cc4e522bf0c705a842b47cef6a24b5b10a8c511
Dragusin Cristian <dragusincristian609@gmail.com>
2025-07-13 00:59:43 +0300

    [USE-DEFERRED-VALUE] expensive search simulator

--- smart-dashbboard/src/components/NotesDashboard/NotesDashboard.tsx
+++ smart-dashbboard/src/components/NotesDashboard/NotesDashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useDeferredValue, useState } from "react";
 import TNote from "types/Note.type";
 import NoteCard from "./components/NoteCard";
 import ListView from "components/common/ListView";
@@ -14,11 +14,37 @@ const NotesDashboard: React.FC<CrudListInjectedProps<TNote>> = ({
   editItem: editNote,
   addItem: addNote,
 }) => {
+  const [query, setQuery] = useState("");
+  const deferredQuery = useDeferredValue(query);
+
+  const filteredNotes = expensiveFilterFunction(deferredQuery);
+
+  function expensiveFilterFunction(query: string) {
+    // Simulate expensive work. For example fethcing from API:
+    const start = performance.now();
+    while (performance.now() - start < 200) {
+      // Busy-wait to simulate slowness
+    }
+    if (!query) return notes;
+    return notes.filter((note) =>
+      note.title.toLowerCase().includes(query.toLowerCase())
+    );
+  }
+
   return (
     <div>
       <Text as="heading">Notes Dashborad</Text>
+      <Text>
+        <label htmlFor="expensive search">Expensive search simulator: </label>
+        <input
+          name="expensive search"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search..."
+        />
+      </Text>
       <ListView<TNote>
-        data={notes}
+        data={filteredNotes}
         renderItem={(note) => (
           <AnimatedLayout>
             <NoteCard
