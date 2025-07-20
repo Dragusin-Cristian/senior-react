ee7acdcac805a2286e0daf0cb1d46d63b59b75be
Dragusin Cristian <dragusincristian609@gmail.com>
2025-07-21 00:44:20 +0300

    build scss into css [FINISHED SECTION 2: CSS ARCHITECTURE]

---
+++ scss/src/scripts/build.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const path = require("path");
+const sass = require("sass");
+
+const getComponents = () => {
+  let allComponents = [];
+
+  const types = ["atoms", "molecules", "organisms"];
+
+  types.forEach((type) => {
+    const allFiles = fs.readdirSync(`src/${type}`).map((file) => ({
+      input: `src/${type}/${file}`,
+      output: `lib/${file.replace(/\.scss$/, ".css")}`,
+    }));
+
+    allComponents = [...allComponents, ...allFiles];
+  });
+
+  return allComponents;
+};
+
+const compile = (inputPath, outputPath) => {
+  const result = sass.compile(path.resolve(inputPath), {
+    style: "expanded",
+    loadPaths: [path.resolve("src")],
+  });
+
+  fs.writeFileSync(path.resolve(outputPath), result.css);
+};
+
+try {
+  fs.mkdirSync(path.resolve("lib"), { recursive: true });
+} catch (e) {
+  console.error("Error creating lib directory:", e);
+}
+
+compile("src/global.scss", "lib/global.css");
+
+getComponents().forEach((component) => {
+  compile(component.input, component.output);
+});
