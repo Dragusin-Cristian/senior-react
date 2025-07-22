ee8683cf7acb4091ae088048560ecf38bee0eb4b
Dragusin Cristian <dragusincristian609@gmail.com>
2025-07-22 22:02:20 +0300

    Select component

--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -4,17 +4,23 @@ import ReactDOM from "react-dom/client";
 import "@ds.e/scss/lib/Utilities.css";
 import "@ds.e/scss/lib/Text.css";
 import "@ds.e/scss/lib/Margin.css";
+import "@ds.e/scss/lib/Select.css";
 import "@ds.e/scss/lib/global.css";
 
-import { Text, Color, Margin } from "@ds.e/react";
+import { Margin, Select } from "@ds.e/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <div>
-    <Margin left>
-      <Text size={"xs"}>this is some text</Text>
+    <Margin top right bottom left space="lg">
+      <Select
+        options={[
+          { label: "value 1", value: "Banana" },
+          { label: "value 2", value: "Orange" },
+        ]}
+      />
     </Margin>
   </div>
 );
